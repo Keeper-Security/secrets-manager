174386cd9d4d6078cd45566185520cd6dfa038ca
John Walstra <jwalstra@keepersecurity.com>
2021-05-26 16:49:20 -0500

    Ansible Plugin
    See the GitBook page for how to use.

---
+++ sdk/python/ansible/tests/keeper_ansible_test.py
@@ -0,0 +1,76 @@
+import unittest
+from unittest.mock import patch
+import os
+import sys
+import tempfile
+import json
+
+from keeper_ansible import KeeperAnsible
+
+
+def mocked_commander_get_secrets():
+    return []
+
+
+class KeeperAnsibleTest(unittest.TestCase):
+
+    def setUp(self):
+
+        # Add in addition Python libs. This includes the base
+        # module for Keeper Ansible and the Keeper SDK.
+        base_dir = os.path.dirname(os.path.realpath(__file__))
+        sys.path.append(os.path.join(base_dir, "..", "modules"))
+        sys.path.append(os.path.join(base_dir, "..", "..", "core"))
+
+    @patch("keepercommandersm.Commander.get_secrets", side_effect=mocked_commander_get_secrets)
+    def test_config_read_file_json_file(self, mock_get_secrets):
+
+        """
+        Create a JSON config file and load it using the Ansible variable where the config file name is
+        specified.
+        """
+
+        with tempfile.NamedTemporaryFile("w", delete=False) as temp_config:
+            config = json.dumps({
+                "server": "dev.keepersecurity.com",
+                "appKey": "4I57jIjbn2OCNwCFzyGHek0YFfhdh2y9TLTncOwujmM",
+                "clientId": "yccmFNJ9X6hKZerHWPipakDWHYCgCjjP86zwKM9N94Y",
+                "clientKey": "KmsOqSq-aB0l7VraWHBHhMaZC2HYDDY5rJIgaP3qD7E",
+                "privateKey": "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgf54pIB7q_qV-B_2Zlw5NPZybvQzAHfZQFl4veRG"
+                              "Z3YChRANCAASv7xdTLFfRuCIur0GUFIp1rPERmuBgtmUlSq72kYBwFO-NEbTjC9JUcZbQ73kDiGdvYwiuQqkKc7"
+                              "q4aHc_zpzH"
+            })
+            temp_config.write(config)
+            temp_config.seek(0)
+
+            keeper_config_file_key = KeeperAnsible.keeper_key(KeeperAnsible.KEY_CONFIG_FILE_SUFFIX)
+
+            KeeperAnsible(
+                task_vars={
+                    keeper_config_file_key: temp_config.name,
+                    "keeper_verify_ssl_certs": False
+                }
+            )
+            mock_get_secrets.assert_called_once()
+
+    @patch("keepercommandersm.Commander.get_secrets", side_effect=mocked_commander_get_secrets)
+    def test_config_in_ansible_task_vars(self, mock_get_secrets):
+
+        values = {
+            "server": "dev.keepersecurity.com",
+            "app_key": "hDlYsYMPNJMhJb7d_Ca4u1yl5RSUzCvuIsFx32h7t04",
+            "client_id": "PrF-9pxKIiLzngZb31GpimYXMhQgO9w1jO8PwfBD554",
+            "client_key": "1zOuIXXkrrAaDioo6MX8yohRBC0otcBXF58Th5D0LOs",
+            "private_key": "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgAPoRbTN0Nt7Go_nVMaSY8dgLFzvYmaH5_14JoyX-"
+                           "z-ShRANCAATiHRnQxz4f3bAS8eFuhaaEDnbwZF3OLKbf7A_ZshyoPPqdKiEX-XU9dCTF3f0I-QPusqCzlUuAVu8J"
+                           "UFNPvoJJ"
+        }
+
+        task_vars = {
+            "keeper_verify_ssl_certs": False
+        }
+        for key in values:
+            task_vars[KeeperAnsible.keeper_key(key)] = values[key]
+
+        KeeperAnsible(task_vars=task_vars)
+        mock_get_secrets.assert_called_once()
---
+++ sdk/python/ansible/tests/keeper_get_test.py
@@ -0,0 +1,80 @@
+import unittest
+from unittest.mock import patch
+import os
+import sys
+from .ansible_test_framework import AnsibleTestFramework, RecordMaker
+import tempfile
+
+
+records = {
+    "EG6KdJaaLG7esRZbMnfbFA": RecordMaker.make_record(
+        uid="EG6KdJaaLG7esRZbMnfbFA",
+        title="JW-F1-R1",
+        value="aaa"
+    ),
+    "TRd_567FkHy-CeGsAzs8aA": RecordMaker.make_record(
+        uid="TRd_567FkHy-CeGsAzs8aA",
+        title="JW-F1-R1",
+        value="ddd"
+    ),
+    "A_7YpGBUgRTeDEQLhVRo0Q": RecordMaker.make_file(
+        uid="A_7YpGBUgRTeDEQLhVRo0Q",
+        title="JW-F1-R2-File",
+        files=[
+            {"name": "nailing it.mp4", "type": "video/mp4", "url": "http://localhost/abc", "data": "ABC123"},
+            {"name": "video_file.mp4", "type": "video/mp4", "url": "http://localhost/xzy", "data": "XYZ123"},
+        ]
+    )
+}
+
+
+def mocked_commander_get_secrets(*args):
+
+    if len(args) > 0:
+        uid = args[0][0]
+        ret = [records[uid]]
+    else:
+        ret = [records[x] for x in records]
+    return ret
+
+
+class KeeperGetTest(unittest.TestCase):
+
+    def setUp(self):
+
+        # Add in addition Python libs. This includes the base
+        # module for Keeper Ansible and the Keeper SDK.
+        self.base_dir = os.path.dirname(os.path.realpath(__file__))
+        sys.path.append(os.path.join(self.base_dir, "..", "modules"))
+        sys.path.append(os.path.join(self.base_dir, "..", "..", "core"))
+
+        self.ansible_base_dir = os.path.join(self.base_dir, "ansible_example")
+
+    def _common(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            a = AnsibleTestFramework(
+                base_dir=self.ansible_base_dir,
+                playbook=os.path.join("playbooks", "keeper_get.yml"),
+                inventory=os.path.join("inventory", "all"),
+                plugin_base_dir=os.path.join(self.base_dir, "..", "plugins"),
+                vars={
+                    "tmp_dir": temp_dir,
+                    "uid": "TRd_567FkHy-CeGsAzs8aA"
+                }
+            )
+            r, out, err = a.run()
+            print(out)
+            result = r[0]["localhost"]
+            self.assertEqual(result["ok"], 3, "3 things didn't happen")
+            self.assertEqual(result["failures"], 0, "failures was not 0")
+            self.assertEqual(result["changed"], 0, "0 things didn't change")
+            self.assertRegex(out, r'password_ddd', "Did not find the password in the stdout")
+
+    #@unittest.skip
+    @patch("keepercommandersm.Commander.get_secrets", side_effect=mocked_commander_get_secrets)
+    def test_keeper_get_mock(self, mock_commander_get_secrets):
+        self._common()
+
+    @unittest.skip
+    def test_keeper_get_live(self):
+        self._common()
