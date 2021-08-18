4fb50441f4f63b388ea0712a01b8a6e99947c9c2
Maksim Ustinov <mustinov@keepersecurity.com>
2021-08-18 15:30:24 -0700

    Python SDK: Quick adjustment to examples

--- examples/python/hello-ksm-read.py
+++ examples/python/hello-ksm-read.py
@@ -3,10 +3,18 @@ from keeper_secrets_manager_core.storage import FileKeyValueStorage
 
 if __name__ == '__main__':
 
+    hostname = input("Enter KSM Server\nPress <Enter> to use keepersecurity.com server: ")
+    if not hostname:
+        hostname = "keepersecurity.com"
+
+    token = input("Enter one time token: ")
+    print("Hostname: [" + hostname + "]")
+    print("Token: [" + token + "]")
+
     secrets_manager = SecretsManager(
 ## if your Keeper Account is in other region than US, update the hostname accordingly
-        hostname='keepersecurity.com',
-        token='<ONE TIME TOKEN>',
+        hostname=hostname,
+        token=token,
         config=FileKeyValueStorage(),
     )
 
@@ -14,10 +22,11 @@ if __name__ == '__main__':
 
     for r in all_records:
 
+        # view record data details
         print("\tPassword: %s" % r.password)
+        print("\tRecord details: %s" % r.dict)
 
-        print("\tRecord details:\n%s" % r.dict)
-
+        # view all files in record if present
         count = 0
         for f in r.files:
             count = count + 1
