dfce0fb6101b6487cbcfae28790306b2056eedbc
John Walstra <70371225+jsupun@users.noreply.github.com>
2021-10-19 11:02:21 -0500

    Fix region token (#154)
    * Split the region and token
    
    * Bump version

--- integration/keeper_secrets_manager_ansible/tests/keeper_ansible_test.py
+++ integration/keeper_secrets_manager_ansible/tests/keeper_ansible_test.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest.mock import patch
 import os
-import sys
 import tempfile
 import json
 
@@ -22,6 +21,15 @@ class KeeperAnsibleTest(unittest.TestCase):
         # Add in addition Python libs. This includes the base
         # module for Keeper Ansible and the Keeper SDK.
         self.base_dir = os.path.dirname(os.path.realpath(__file__))
+
+        # Make sure the a config file does not already exists.
+        if os.path.exists("client-config.json") is True:
+            os.unlink("client-config.json")
+
+    def tearDown(self):
+        # Clean up
+        if os.path.exists("client-config.json") is True:
+            os.unlink("client-config.json")
 
     @patch("keeper_secrets_manager_core.core.SecretsManager.get_secrets", side_effect=get_secrets)
     def test_config_read_file_json_file(self, mock_get_secrets):
@@ -79,6 +87,20 @@ class KeeperAnsibleTest(unittest.TestCase):
         ka = KeeperAnsible(task_vars=task_vars)
         ka.client.get_secrets()
         mock_get_secrets.assert_called_once()
+
+    @patch("keeper_secrets_manager_core.core.SecretsManager.get_secrets", side_effect=get_secrets)
+    def test_ansible_cli_init(self, mock_get_secrets):
+
+        stdout = io.StringIO()
+        with redirect_stdout(stdout):
+            main(["--keeper_token", "US:ii-oOUHxy0qHGHF97nd8APYZDVVR07LJj4U76Avcjno"])
+        content = stdout.getvalue()
+        self.assertRegex(content, r'Config file created', 'did not find expected text')
+        with open("client-config.json", "r") as fh:
+            config = json.load(fh)
+            self.assertEqual("ii-oOUHxy0qHGHF97nd8APYZDVVR07LJj4U76Avcjno", config.get("clientKey"))
+            self.assertEqual("US", config.get("hostname"))
+            fh.close()
 
     def test_ansible_cli_version(self):
 
