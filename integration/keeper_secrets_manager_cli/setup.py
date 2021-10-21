76d446a5157d427a7cf7a8856d139b4115680c7a
John Walstra <jkw2395@icloud.com>
2021-10-21 15:55:02 -0500

    Bump version

--- integration/keeper_secrets_manager_cli/setup.py
+++ integration/keeper_secrets_manager_cli/setup.py
@@ -22,7 +22,7 @@ install_requires = [
 # Version set in the keeper_secrets_manager_cli.version file.
 setup(
     name="keeper-secrets-manager-cli",
-    version="0.0.34",
+    version="0.0.35",
     description="Command line tool for Keeper Secrets Manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
