c64e5c1d5efbf642e6442cd35d5bf170ababe612
Maksim Ustinov <maksimu@gmail.com>
2021-10-27 23:13:18 -0700

    Bumping Java SDK Version to 16.1.1

--- sdk/java/core/build.gradle.kts
+++ sdk/java/core/build.gradle.kts
@@ -6,7 +6,7 @@ import java.util.*
 group = "com.keepersecurity.secrets-manager"
 
 // During publishing, If version ends with '-SNAPSHOT' then it will be published to Maven snapshot repository
-version = "16.1.0"
+version = "16.1.1"
 
 plugins {
     `java-library`
