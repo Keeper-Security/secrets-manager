bc79446410f21b34726d85cb0b8a7590545859aa
Ivan Dimov <idimov@keepersecurity.com>
2021-10-29 18:28:45 -0500

    fixed temp file handling on windows

--- sdk/python/core/tests/smoke_test.py
+++ sdk/python/core/tests/smoke_test.py
@@ -30,135 +30,140 @@ class SmokeTest(unittest.TestCase):
 
         """
 
-        with tempfile.NamedTemporaryFile("w") as fh:
-            fh.write(
-                json.dumps({
-                    "hostname": "fake.keepersecurity.com",
-                    "appKey": "9vVajcvJTGsa2Opc_jvhEiJLRKHtg2Rm4PAtUoP3URw=",
-                    "clientId": "Ae3589ktgynN6vvFtBwlsAbf0fHhXCcf7JqtKXK/3UCE"
-                                "LujQuYuXvFFP08d2rb4aQ5Z4ozgD2yek9sjbWj7YoQ==",
-                    "clientKey": "zKoSCC6eNrd3N9CByRBsdChSsTeDEAMvNj9Bdh7BJuo",
-                    "privateKey": "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgaKWvicgtslVJKJU-_LBMQQGfJAycwOtx9d"
-                                  "jH0YEvBT-hRANCAASB1L44QodSzRaIOhF7f_2GlM8Fg0R3i3heIhMEdkhcZRDLxIGEeOVi3otS0UBFTrbE"
-                                  "T6joq0xCjhKMhHQFaHYI"
-                })
-            )
-            fh.seek(0)
-            secrets_manager = SecretsManager(config=FileKeyValueStorage(config_file_location=fh.name))
+        try:
+            with tempfile.NamedTemporaryFile("w", delete=False) as fh:
+                fh.write(
+                    json.dumps({
+                        "hostname": "fake.keepersecurity.com",
+                        "appKey": "9vVajcvJTGsa2Opc_jvhEiJLRKHtg2Rm4PAtUoP3URw=",
+                        "clientId": "Ae3589ktgynN6vvFtBwlsAbf0fHhXCcf7JqtKXK/3UCE"
+                                    "LujQuYuXvFFP08d2rb4aQ5Z4ozgD2yek9sjbWj7YoQ==",
+                        "clientKey": "zKoSCC6eNrd3N9CByRBsdChSsTeDEAMvNj9Bdh7BJuo",
+                        "privateKey": "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgaKWvicgtslVJKJU-_LBMQQGfJAycwOtx9d"
+                                    "jH0YEvBT-hRANCAASB1L44QodSzRaIOhF7f_2GlM8Fg0R3i3heIhMEdkhcZRDLxIGEeOVi3otS0UBFTrbE"
+                                    "T6joq0xCjhKMhHQFaHYI"
+                    })
+                )
+                fh.seek(0)
+                secrets_manager = SecretsManager(config=FileKeyValueStorage(config_file_location=fh.name))
 
-            # --------------------------
-            # Add three records, 2 outside a folder, 1 inside folder
+                # --------------------------
+                # Add three records, 2 outside a folder, 1 inside folder
 
-            res_1 = mock.Response()
+                res_1 = mock.Response()
 
-            one = res_1.add_record(title="My Record 1")
-            one.field("login", "My Login 1")
-            one.field("password", "My Password 1")
-            one.custom_field("My Custom 1", "custom1")
+                one = res_1.add_record(title="My Record 1")
+                one.field("login", "My Login 1")
+                one.field("password", "My Password 1")
+                one.custom_field("My Custom 1", "custom1")
 
-            # The frontend allows for custom field to not have unique names :(. The best way we
-            # can handle this is to set label and field type.
-            one.custom_field("My Custom 2", "custom2")
-            one.custom_field("My Custom 2", "my secret", field_type='secret')
+                # The frontend allows for custom field to not have unique names :(. The best way we
+                # can handle this is to set label and field type.
+                one.custom_field("My Custom 2", "custom2")
+                one.custom_field("My Custom 2", "my secret", field_type='secret')
 
-            two = res_1.add_record(title="My Record 2")
-            two.field("login", "My Login 2")
-            two.field("password", "My Password 2")
-            two.add_file("My File 1")
-            two.add_file("My File 2")
+                two = res_1.add_record(title="My Record 2")
+                two.field("login", "My Login 2")
+                two.field("password", "My Password 2")
+                two.add_file("My File 1")
+                two.add_file("My File 2")
 
-            folder = res_1.add_folder()
-            three = folder.add_record(title="My Record 3")
-            three.field("login", "My Login 3")
-            three.field("password", "My Password 3")
+                folder = res_1.add_folder()
+                three = folder.add_record(title="My Record 3")
+                three.field("login", "My Login 3")
+                three.field("password", "My Password 3")
 
-            # --------------------------
+                # --------------------------
 
-            res_2 = mock.Response()
+                res_2 = mock.Response()
 
-            # Use the existing first record of res_1
-            res_2.add_record(record=one)
+                # Use the existing first record of res_1
+                res_2.add_record(record=one)
 
-            # --------------------------
+                # --------------------------
 
-            res_queue = mock.ResponseQueue(client=secrets_manager)
+                res_queue = mock.ResponseQueue(client=secrets_manager)
 
-            # All records
-            res_queue.add_response(res_1)
-            # Single record
-            res_queue.add_response(res_2)
+                # All records
+                res_queue.add_response(res_1)
+                # Single record
+                res_queue.add_response(res_2)
 
-            # Save response
-            res_queue.add_response(mock.Response(content=""))
-            # Save with error
-            # Make the error message
-            error_json = {
-                "path": "https://fake.keepersecurity.com/api/rest/sm/v1/get_secret, POST, python-requests/2.25.1",
-                "additional_info": "",
-                "location": "some location",
-                "error": "access_denied",
-                "message": "You can't update because of spite"
-            }
-            res_queue.add_response(mock.Response(content=json.dumps(error_json).encode(), status_code=403))
-            # --------------------------
-            # DO THE WORKS
+                # Save response
+                res_queue.add_response(mock.Response(content=""))
+                # Save with error
+                # Make the error message
+                error_json = {
+                    "path": "https://fake.keepersecurity.com/api/rest/sm/v1/get_secret, POST, python-requests/2.25.1",
+                    "additional_info": "",
+                    "location": "some location",
+                    "error": "access_denied",
+                    "message": "You can't update because of spite"
+                }
+                res_queue.add_response(mock.Response(content=json.dumps(error_json).encode(), status_code=403))
+                # --------------------------
+                # DO THE WORKS
 
-            records = secrets_manager.get_secrets()
-            self.assertEqual(len(records), 3, "didn't get 3 records")
+                records = secrets_manager.get_secrets()
+                self.assertEqual(len(records), 3, "didn't get 3 records")
 
-            records = secrets_manager.get_secrets([one.uid])
-            self.assertEqual(len(records), 1, "didn't get 1 records")
-            record = records[0]
+                records = secrets_manager.get_secrets([one.uid])
+                self.assertEqual(len(records), 1, "didn't get 1 records")
+                record = records[0]
 
-            # Test field gets
-            login = record.field("login", single=True)
-            self.assertEqual(login, "My Login 1", "didn't get the correct login")
-            login_values = record.field("login")
-            self.assertEqual(len(login_values), 1, "didn't find only 1 login")
-            self.assertEqual(login_values[0], "My Login 1", "didn't get the correct login in array")
+                # Test field gets
+                login = record.field("login", single=True)
+                self.assertEqual(login, "My Login 1", "didn't get the correct login")
+                login_values = record.field("login")
+                self.assertEqual(len(login_values), 1, "didn't find only 1 login")
+                self.assertEqual(login_values[0], "My Login 1", "didn't get the correct login in array")
 
-            # Test custom field gets
-            custom = record.custom_field("My Custom 1", single=True)
-            self.assertEqual(custom, "custom1", "didn't get the correct My Custom 1 value")
-            custom = record.custom_field("My Custom 2", field_type='text')
-            self.assertEqual(custom[0], "custom2", "didn't get the correct My Custom 2/text value")
-            custom = record.custom_field("My Custom 2", field_type='secret')
-            self.assertNotEqual(custom[0], "my secret", "should have gotten the first My Custom 2 record")
+                # Test custom field gets
+                custom = record.custom_field("My Custom 1", single=True)
+                self.assertEqual(custom, "custom1", "didn't get the correct My Custom 1 value")
+                custom = record.custom_field("My Custom 2", field_type='text')
+                self.assertEqual(custom[0], "custom2", "didn't get the correct My Custom 2/text value")
+                custom = record.custom_field("My Custom 2", field_type='secret')
+                self.assertNotEqual(custom[0], "my secret", "should have gotten the first My Custom 2 record")
 
-            # Test field sets
-            record.field("login", value="ABC")
-            self.assertEqual(record.field("login", single=True), "ABC", "didn't get the correct login for str")
-            record.field("login", value=["XYZ"])
-            self.assertEqual(record.field("login", single=True), "XYZ", "didn't get the correct login for array")
+                # Test field sets
+                record.field("login", value="ABC")
+                self.assertEqual(record.field("login", single=True), "ABC", "didn't get the correct login for str")
+                record.field("login", value=["XYZ"])
+                self.assertEqual(record.field("login", single=True), "XYZ", "didn't get the correct login for array")
 
-            # Test custom field sets
-            record.custom_field("My Custom 1", "NEW VALUE")
-            custom = record.custom_field("My Custom 1", single=True)
-            self.assertEqual(custom, "NEW VALUE", "didn't get the correct My Custom 1 value after set")
+                # Test custom field sets
+                record.custom_field("My Custom 1", "NEW VALUE")
+                custom = record.custom_field("My Custom 1", single=True)
+                self.assertEqual(custom, "NEW VALUE", "didn't get the correct My Custom 1 value after set")
 
-            # SAVE THE RECORD
+                # SAVE THE RECORD
 
-            secrets_manager.save(record)
-
-            # While we are here, save again with error response
-            try:
                 secrets_manager.save(record)
-                self.fail("The second save should have failed but didn't")
-            except KeeperError as err:
-                self.assertRegex(str(err), r"You can't update", "did not get correct exception message")
-            except Exception as err:
-                self.fail("Did not get correct exception: {}".format(err))
 
-            # Take the save record and queue it back up as a response.
-            saved_res = mock.Response()
-            saved_res.add_record(keeper_record=record)
-            res_queue.add_response(saved_res)
+                # While we are here, save again with error response
+                try:
+                    secrets_manager.save(record)
+                    self.fail("The second save should have failed but didn't")
+                except KeeperError as err:
+                    self.assertRegex(str(err), r"You can't update", "did not get correct exception message")
+                except Exception as err:
+                    self.fail("Did not get correct exception: {}".format(err))
 
-            records = secrets_manager.get_secrets([record.uid])
-            self.assertEqual(len(records), 1, "didn't get 1 records")
-            record = records[0]
-            custom = record.custom_field("My Custom 1", single=True)
-            self.assertEqual(custom, "NEW VALUE", "didn't get the correct My Custom 1 value after write")
+                # Take the save record and queue it back up as a response.
+                saved_res = mock.Response()
+                saved_res.add_record(keeper_record=record)
+                res_queue.add_response(saved_res)
+
+                records = secrets_manager.get_secrets([record.uid])
+                self.assertEqual(len(records), 1, "didn't get 1 records")
+                record = records[0]
+                custom = record.custom_field("My Custom 1", single=True)
+                self.assertEqual(custom, "NEW VALUE", "didn't get the correct My Custom 1 value after write")
+        finally:
+            try: os.unlink(fh.name)
+            except Exception: pass
+
 
     def test_verify_ssl_certs(self):
 
