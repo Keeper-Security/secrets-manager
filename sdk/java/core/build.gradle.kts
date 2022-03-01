29211b153a7f6c41d007d4ad96174954f0965116
Maksim Ustinov <maksimu@gmail.com>
2022-02-28 16:24:58 -0800

    Releasing Java SDK 16.3.0

--- sdk/java/core/build.gradle.kts
+++ sdk/java/core/build.gradle.kts
@@ -6,7 +6,7 @@ import java.util.*
 group = "com.keepersecurity.secrets-manager"
 
 // During publishing, If version ends with '-SNAPSHOT' then it will be published to Maven snapshot repository
-version = "16.3.0-SNAPSHOT"
+version = "16.3.0"
 
 plugins {
     `java-library`
