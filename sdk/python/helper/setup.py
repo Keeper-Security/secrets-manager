e54e842f9ef69164375518344a9383000604daa1
John Walstra <70371225+jsupun@users.noreply.github.com>
2022-03-23 15:07:17 -0500

    Fix cli app helper (#240)
    * Add debug to find location of modules.
    
    * List modules that were installed
    
    * Print the location of module
    
    * Print locate of module
    
    * Attempt to load the default records from the current directory
    
    * Figure out which directory we are in
    
    * Current directory was $HOME. Get info about current module location
    
    * Handle both module installl and binary install
    
    * Clean up code
    
    To find the YML in the binary, walk down the directory tree looking for
    the file. If you get to the root directory, the file can't be found. Made
    this work for Unix and Windows.

--- sdk/python/helper/setup.py
+++ sdk/python/helper/setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name="keeper-secrets-manager-helper",
-    version="1.0.1",
+    version="1.0.2",
     description="Keeper Secrets Manager SDK helper for managing records.",
     long_description=long_description,
     long_description_content_type="text/markdown",
