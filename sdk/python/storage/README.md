1566de6d38a1278172bbd441398b9639e17f2499
idimov-keeper <78815270+idimov-keeper@users.noreply.github.com>
2022-09-27 18:37:37 -0500

    Storage hsm (#357)
    Initial release of the Python Storages

---
+++ sdk/python/storage/README.md
@@ -0,0 +1,10 @@
+# Keeper Secrets Manager Storage
+
+The Keeper Secrets Manager Storage module for working with custom key-value storages, creating and managing configuration files. To be used with keeper-secrets-manager-core.
+
+For more information see our official documentation page https://docs.keeper.io/secrets-manager/secrets-manager
+
+# Change Log
+
+## 1.0.0
+- Initial release
---
+++ sdk/python/storage/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README.md file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = [
+    'keeper-secrets-manager-core>=16.3.5'
+]
+
+setup(
+    name="keeper-secrets-manager-storage",
+    version="1.0.0",
+    description="Keeper Secrets Manager SDK helper for managing configurations key-value storage.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Keeper Security",
+    author_email="sm@keepersecurity.com",
+    url="https://github.com/Keeper-Security/secrets-manager",
+    license="MIT",
+    keywords="Keeper Password Secrets Manager Storage Key-Value HSM",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    zip_safe=False,
+    package_data={},
+    include_package_data=True,
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    project_urls={
+        "Bug Tracker": "https://github.com/Keeper-Security/secrets-manager/issues",
+        "Documentation": "https://docs.keeper.io/secrets-manager/secrets-manager/overview",
+        "Source Code": "https://github.com/Keeper-Security/secrets-manager",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Security",
+    ]
+)
