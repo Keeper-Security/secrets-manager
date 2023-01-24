08adb5690b67d561650fa4ed9d3a08edd4e0d461
idimov-keeper <78815270+idimov-keeper@users.noreply.github.com>
2023-01-24 15:11:02 -0600

    KSM-316 - Notation improvements - new parser, notation URIs using record title,… (#389)
    * Notation improvements - new parser, notation URIs using record title, new escape characters
    
    * upgraded tests to net6.0
    
    * Improved notation search for files
    
    * Added GetNotation to powershell

--- sdk/dotNet/SecretsManager.Test.Core/Notation.Test.cs
+++ sdk/dotNet/SecretsManager.Test.Core/Notation.Test.cs
@@ -6,14 +6,14 @@ namespace SecretsManager.Test
     public class NotationTests
     {
         private const string RecordUid = "k9qMpcO0aszz9w3li5XbaQ";
+        private const string testRecordDataJson =
+            "{\"title\":\"My Record 1\",\"type\":\"login\",\"fields\":[{\"type\":\"login\",\"value\":[\"My Login 1\"]},{\"type\":\"password\",\"value\":[\"My Password 1\"]}],\"custom\":[{\"type\":\"text\",\"label\":\"My Custom 1\",\"value\":[\"custom1\"]},{\"type\":\"text\",\"label\":\"My Custom 1\",\"value\":[\"custom1\"]},{\"type\":\"text\",\"label\":\"My Custom 2\",\"value\":[\"one\",\"two\",\"three\"]},{\"type\":\"phone\",\"label\":\"phone\",\"value\":[{\"number\":\"555-5555555\",\"ext\":\"55\"},{\"number\":\"777-7777777\",\"ext\":\"77\"},{\"number\":\"888-8888888\",\"ext\":\"\",\"type\":\"Home\"},{\"number\":\"999-9999999\",\"type\":\"Work\"}]},{\"type\":\"name\",\"label\":\"name\",\"value\":[{\"first\":\"Jenny\",\"middle\":\"X\",\"last\":\"Smith\"}]}]}";
 
         [Test]
         public void NotationsWork()
         {
-            const string testRecordDataJson =
-                "{\"title\":\"My Record 1\",\"type\":\"login\",\"fields\":[{\"type\":\"login\",\"value\":[\"My Login 1\"]},{\"type\":\"password\",\"value\":[\"My Password 1\"]}],\"custom\":[{\"type\":\"text\",\"label\":\"My Custom 1\",\"value\":[\"custom1\"]},{\"type\":\"text\",\"label\":\"My Custom 1\",\"value\":[\"custom1\"]},{\"type\":\"text\",\"label\":\"My Custom 2\",\"value\":[\"one\",\"two\",\"three\"]},{\"type\":\"phone\",\"label\":\"phone\",\"value\":[{\"number\":\"555-5555555\",\"ext\":\"55\"},{\"number\":\"777-7777777\",\"ext\":\"77\"},{\"number\":\"888-8888888\",\"ext\":\"\",\"type\":\"Home\"},{\"number\":\"999-9999999\",\"type\":\"Work\"}]},{\"type\":\"name\",\"label\":\"name\",\"value\":[{\"first\":\"Jenny\",\"middle\":\"X\",\"last\":\"Smith\"}]}]}";
             var recordData = JsonUtils.ParseJson<KeeperRecordData>(CryptoUtils.StringToBytes(testRecordDataJson));
-            var secrets = new KeeperSecrets(null,null, new[]
+            var secrets = new KeeperSecrets(null, null, new[]
             {
                 new KeeperRecord(null, RecordUid, null, null, recordData, 0, null)
             });
@@ -66,6 +66,55 @@ namespace SecretsManager.Test
 
             value = Notation.GetValue(secrets, $"keeper://{RecordUid}/custom_field/name[last]");
             Assert.AreEqual("Smith", value);
+        }
+
+        [Test]
+        public void NotationParserWork()
+        {
+            try
+            {
+                Notation.ParseNotation("/file"); // file requires parameters
+                Assert.Fail("Parsing bad notation did not throw");
+            }
+            catch (Exception) { }
+
+            try
+            {
+                Notation.ParseNotation("/type/extra"); // extra characters after last section
+                Assert.Fail("Parsing bad notation did not throw");
+            }
+            catch (Exception) { }
+
+            var res = Notation.ParseNotation("/type");
+            Assert.AreEqual("type", res[2].Text.Item1);
+
+            res = Notation.ParseNotation("/title");
+            Assert.AreEqual("title", res[2].Text.Item1);
+
+            res = Notation.ParseNotation("/notes");
+            Assert.AreEqual("notes", res[2].Text.Item1);
+
+            res = Notation.ParseNotation("/file/filename.ext");
+            Assert.AreEqual("file", res[2].Text.Item1);
+            Assert.AreEqual("filename.ext", res[2].Parameter.Item1);
+
+            res = Notation.ParseNotation("/field/text");
+            Assert.AreEqual("field", res[2].Text.Item1);
+            Assert.AreEqual("text", res[2].Parameter.Item1);
+
+            res = Notation.ParseNotation(@"/custom_field/label with \[[0][middle]");
+            Assert.AreEqual("", res[1].Text.Item1); // empty title
+            Assert.AreEqual("custom_field", res[2].Text.Item1);
+            Assert.AreEqual(@"label with [", res[2].Parameter.Item1);
+            Assert.AreEqual("0", res[2].Index1.Item1);
+            Assert.AreEqual("middle", res[2].Index2.Item1);
+
+            res = Notation.ParseNotation(@"title with \[\]\//custom_field/label with \[[0][middle]");
+            Assert.AreEqual(@"title with []/", res[1].Text.Item1);
+            Assert.AreEqual("custom_field", res[2].Text.Item1);
+            Assert.AreEqual(@"label with [", res[2].Parameter.Item1);
+            Assert.AreEqual("0", res[2].Index1.Item1);
+            Assert.AreEqual("middle", res[2].Index2.Item1);
         }
     }
 }
