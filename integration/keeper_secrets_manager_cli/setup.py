505bd6d96f8d9732aef77a92fcc6ed1a141c3747
Ivan Dimov <78815270+idimov-keeper@users.noreply.github.com>
2023-05-30 15:19:32 -0500

    added boto3 dependency

--- integration/keeper_secrets_manager_cli/setup.py
+++ integration/keeper_secrets_manager_cli/setup.py
@@ -20,7 +20,8 @@ install_requires = [
     'importlib_metadata',
     'pyyaml',
     'update-checker',
-    'psutil'
+    'psutil',
+    'boto3'
 ]
 
 # Version set in the keeper_secrets_manager_cli.version file.
