f5f8be6754de8d7daa5171c7a49fca7ef29279ca
Ivan Dimov <78815270+idimov-keeper@users.noreply.github.com>
2023-05-31 16:18:03 -0500

    fixed ansible vesion at 7.6.0

--- integration/keeper_secrets_manager_ansible/setup.py
+++ integration/keeper_secrets_manager_ansible/setup.py
@@ -12,7 +12,7 @@ install_requires = [
     'keeper-secrets-manager-core>=16.4.1',
     'keeper-secrets-manager-helper>=1.0.4',
     'importlib_metadata',
-    'ansible'
+    'ansible~=7.6.0'
 ]
 
 setup(
