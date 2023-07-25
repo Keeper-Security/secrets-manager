3afd886e7a32bf9eee3dfddf5bd918b3a365be92
idimov-keeper <78815270+idimov-keeper@users.noreply.github.com>
2023-07-25 01:23:43 -0500

    KSM-199 ServiceNow plugin version 0.0.1 (#492)
    * KSM-199 ServiceNow plugin version 0.0.1

---
+++ integration/servicenow-external-credential-resolver/README.md
@@ -0,0 +1,68 @@
+# MID Server External Credential Resolver for Keeper Secrets Manager
+This is the ServiceNow MID Server custom external credential resolver for the Keeper vault credential storage.
+
+# Pre-requisites:
+Keeper External Credential Resolver requires JDK 1.8 or newer  
+IntelliJ IDEA or any equivalent IDE
+
+> ⚠️ JRE 8 versions prior to u161 require strong cryptography (JCE) to be enabled.
+Latest versions of JRE have strong cryptography enabled [by default](https://bugs.openjdk.java.net/browse/JDK-8170157).
+
+# Steps to build
+* Clone this repository.
+* Import the project in IntelliJ IDEA or any IDE.
+* Update MID Server agent path in build.gradle to point to valid MID Server location.
+* Update the code in CredentialResolver.java to customize anything.
+* Use the following gradle command or IDE (IntelliJ or Eclipse) gradle build option to build the jar:
+  > gradle jar  
+* keeper-external-credentials-0.0.1-SNAPSHOT.jar will be generated under target folder.
+
+# Steps to install and use Keeper Secrets Manager as external credential resolver
+
+* Make sure that "External Credential Storage" plugin (com.snc.discovery.external_credentials) is installed in your ServiceNow instance.
+* Import the keeper-external-credentials-0.0.1-SNAPSHOT.jar file from target folder in ServiceNow instance.
+    - Navigate to MID Server – JAR Files
+    - Create a New Record by clicking New
+    - Name it "KeeperCredentialResolver", version 0.0.1 and attach keeper-external-credentials-0.0.1-SNAPSHOT.jar from target folder.
+    - Click Submit
+* Update the _config.xml_ in MID Server with below parameters and restart the MID Server.
+  `<parameter name="ext.cred.keeper.ksm_config" secure="true" value="<ksm-config-base64-string>"/>`
+* Create Credential in the instance with "External credential store" flag activated.
+* Ensure that the "Credential ID" matches a record UID in your Keeper vault.
+* Ensure that the record in the vault contains fields matching the ServiceNow credential record fields - ex. record _type=login_ or any record type with custom fields of _type=hidden_ or _type=text_ with labels matching with the column names in discovery_credential table, where each label is prefixed with  "mid_" (ex. GCP Credential requires a record with two custom fields labelled: mid_email and mid_secret_key)
+
+# Finding records
+Credential ID (credId parameter) passed from MID Server to Credential Resolver must be either a valid record UID (22 alphanumeric characters incl. "-" and "_") or in the following format type:title. The second format allows searches by type only or by title only (or both, but single ":" is invalid combination)
+> ⚠️ When using `type:title` format for the credential make sure there's only one matching record, since multiple matches result in error.
+
+> ⚠️ We recommend using record UID - first to guarantee uniqueness, second to avoid downloading all records with every single request to do a local search by type/title (due to zero knowledge nature of the keeper vault).
+
+#### Examples:
+_(zero or two or more matches result in an error)_  
+Find by Record UID - `ABCDABCDABCDABCDABCDAB`  
+Find by type and title - `login:MyLogin`  
+Find by title - `:MyLogin`  
+Find by type - `login:`
+
+# Mapping fields
+Keeper record types are dynamic and easy to customize, but there are no specific record types matching corresponding credential types in ServiceNow. Keeper External Credential Resolver uses custom field labels to match record data with MID Server's table columns (`discovery_credential` _table_) just label all required custom fields to match the table columns for a given credential type and prefix that label with "mid_" _(see below how to configure custom prefix)_  
+Credential types that require username/password should use Login records, and add any custom fields required by the credential type - ex. type=hidden label="mid_pkey"  
+Any other types that may not have username/password it is best to use File/Photo records which don't have any standard fields that makes it easier to navigate the custom fields.  
+
+To change the custom field labels prefix update the _config.xml_ in MID Server with the parameters below and restart the MID Server.
+  `<parameter name="ext.cred.keeper.ksm_label_prefix" value="mid_"/>`
+
+> ️ⓘ Use custom fields with type `text`, `multiline` or `hidden` depending on the visibility you want in your Keeper Vault.
+
+> ⚠️ When Login record type is used any custom fields for username/password are ignored _(even if properly labeled mid_user, mid_pswd)_ as these values always come from the Login record type standard fields - Login/Password.
+
+#### Examples:  
+ + Credential type `jdbc` map to Keeper record type `Login` (using standard Login/Password field)
+ + Credential type `api_key` map to Keeper record type `Login` and manually add custom fields with of type `hidden` with labels `mid_ssh_private_key`, and `mid_ssh_passphrase` (_optional_)
+ + Credential type `gcp` map to Keeper record type `File Attachment/Photo` and manually add the required custom fields `mid_email` - _text_, `mid_secret_key` - _hidden_.   
+
+When used with **"External credential store"** option output values must conform to IExternalCredential interface from `snc-automation-api.jar` (values start with VAL_ prefix).
+Currently supported values - should be prefixed with `mid_` in Keeper records to be extracted (Utah: _user, pswd, passphrase, pkey, authprotocol, authkey, privprotocol, privkey, secret_key, client_id, tenant_id, email_)
+
+When used as Custom External Credential Resolver any field could be mapped **if properly prefixed** in Keeper vault and present in corresponding credential type. 
+The credential map returned from the resolve method is expected to have keys matching with the column names in discovery_credential table _ex. sn_cfg_ansible, sn_disco_certmgmt_certificate_ca, cfg_chef_credentials, etc._
---
+++ integration/servicenow-external-credential-resolver/src/main/java/com/snc/discovery/CredentialResolver.java
@@ -0,0 +1,278 @@
+package com.snc.discovery;
+
+import com.snc.automation_common.integration.creds.IExternalCredential;
+import com.snc.core_automation_common.logging.Logger;
+import com.snc.core_automation_common.logging.LoggerFactory;
+
+import com.keepersecurity.secretsManager.core.*;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Basic implementation of a CredentialResolver that uses KSM SDK to connect to Keeper vault.
+ */
+public class CredentialResolver implements IExternalCredential {
+    // Required parameters that must be in the config file in order to use KSM.
+    // Parameters used to access the vault / credentials
+    // KSM Config as base64 string
+    public static final String KSM_CONFIG = "ext.cred.keeper.ksm_config";
+    private static final String KSM_LABEL_PREFIX = "ext.cred.keeper.ksm_label_prefix";
+    private static final String DEF_KSM_LABEL_PREFIX = "mid_";
+
+    // credId is either record UID (without ':') or type:title
+    private static final String DEF_CREDID_SPLIT = ":";
+
+    //Load below parameters from MID config parameters.
+    private String ksmConfig = ""; // The KSM config to use as specified in the MID config.xml file
+    private String ksmLabelPrefix = ""; // The KSM label prefix to use as specified in the MID config.xml file
+
+    // Logger object to log messages in agent.log
+    private static final Logger fLogger = LoggerFactory.getLogger(CredentialResolver.class);
+
+    public CredentialResolver() {}
+
+    /**
+     * Return the API version supported by this class.
+     * Note: should be less than 1.1 for external credential resolver.
+     */
+    @Override
+    public String getVersion() {
+        return "0.1";
+    }
+
+    /**
+     * Config method with preloaded config parameters from config.xml.
+     * @param configMap - contains config parameters with prefix "ext.cred" only.
+     */
+    @Override
+    public void config(Map<String, String> configMap) {
+        //Note: To load config parameters from MID config.xml if not available in configMap.
+        //propValue = Config.get().getProperty("<Parameter Name>")
+
+        ksmConfig = configMap.get(KSM_CONFIG);
+        if(isNullOrEmpty(ksmConfig))
+            fLogger.error("[Vault] INFO - CredentialResolver ksmConfig not set!");
+        String maskedConfig = new String(new char[ksmConfig.length()]).replace('\0', '*');
+        fLogger.info("ksmConfig: " + maskedConfig);
+
+        ksmLabelPrefix = configMap.get(KSM_LABEL_PREFIX);
+        if(isNullOrEmpty(ksmLabelPrefix))
+            ksmLabelPrefix = DEF_KSM_LABEL_PREFIX;
+    }
+
+    /*
+    All these are defined in snc-automation-api IExternalCredential:
+    // These are the only permissible names of arguments passed INTO the resolve() method.
+    public static final String ARG_ID = "id"; // credential identifier as configured on the ServiceNow instance
+    public static final String ARG_IP = "ip"; // IPv4 address of the target system (ex. "10.22.231.12")
+    public static final String ARG_TYPE = "type"; // type of credential (ssh, snmp, etc.)
+    public static final String ARG_MID = "mid"; // the MID server making the request
+
+    // Most common permissible names of values returned FROM the resolve() method.
+    public static final String VAL_USER = "user";
+    public static final String VAL_PSWD = "pswd";
+    public static final String VAL_PASSPHRASE = "passphrase";
+    public static final String VAL_PKEY = "pkey";
+    public static final String VAL_AUTHPROTO = "authprotocol";
+    public static final String VAL_AUTHKEY = "authkey";
+    public static final String VAL_PRIVPROTO = "privprotocol";
+    public static final String VAL_PRIVKEY = "privkey";
+    // for more detailed list check IExternalCredential
+    // ex. email, secret_key, client_id, tenant_id
+    // for a full list check discovery_credential table on the ServiceNow instance
+    // ex. sn_cfg_ansible, sn_disco_certmgmt_certificate_ca, cfg_chef_credentials, etc.
+    // The credential map returned from the resolve method is expected
+    // to have keys matching with the column names in discovery_credential table.
+    */
+
+    /**
+     * Resolve a credential.
+     */
+    @Override
+    public Map<String, String> resolve(Map<String, String> args) {
+        // the resolved credential is returned in a HashMap...
+        Map<String, String> result = new HashMap<>();
+
+        // input params
+        String credId = (String) args.get(ARG_ID);
+        String credType = (String) args.get(ARG_TYPE);
+        String midServer = (String) args.get(ARG_MID);
+
+        if(isNullOrEmpty(credId) || isNullOrEmpty(credType))
+            throw new RuntimeException("Empty credential Id or type found.");
+
+        fLogger.info("midServer: " + midServer);
+        fLogger.info("credType: " + credType);
+        fLogger.info("credId: " + credId);
+
+        // credId is either record UID (without ':') or type:title or :title
+        String recType = "";
+        String recTitle = "";
+        // get record type and title from credId if exists.
+        String[] parts = credId.split(Pattern.quote(DEF_CREDID_SPLIT), 2);
+        if (parts.length == 1) {
+            credId = parts[0].trim();
+            if (!credId.matches("^[A-Za-z0-9_-]{22}$")) {
+                throw new RuntimeException( "Invalid Credential ID: Record UID must be 22 characters, URL Safe Base64 encoded string");
+            }
+        } else if (parts.length == 2) {
+            // empty record type == search by title (any type) - ex. ':MyUniqueRecordTitle'
+            recType = parts[0].trim();
+            // empty title is OK - find a single shared record of the given type - ex. 'login:'
+            recTitle = parts[1]; // leading/trailing spaces matter
+            // ':' is invalid, for records with empty title must provide the record type
+            if(isNullOrEmpty(recType) && isNullOrEmpty(recTitle))
+                throw new RuntimeException( "Invalid Credential ID: Credential Id must provide record type or title or both as a single string - type:title");
+        } else {
+            throw new RuntimeException( "Invalid Credential ID: Credential Id has split string more than twice");
+        }
+
+        List<String> recordsFilter = Collections.<String>emptyList();
+        if(!isNullOrEmpty(recType) || !isNullOrEmpty(recTitle)) {
+            fLogger.info(String.format("recType: '%s', recTitle: '%s'", recType, recTitle));
+        } else {
+            fLogger.info(String.format("Record UID: '%s'", credId));
+            recordsFilter = Collections.singletonList(credId);
+        }
+
+        // Initialize storage
+        InMemoryStorage storage = null;
+        try {
+            storage = new InMemoryStorage(ksmConfig);
+        } catch (Exception e) {
+            fLogger.error("### Error loading KSM Config. Make sure " + KSM_CONFIG +
+                    " contains valid base64 encoded JSON config.", e.getMessage());
+            throw e;
+        }
+
+        try {
+            // Connect to vault and retrieve credential
+            SecretsManagerOptions options = new SecretsManagerOptions(storage);
+            KeeperSecrets secrets = SecretsManager.getSecrets(options, recordsFilter);
+            List<KeeperRecord> records = secrets.getRecords();
+            if (records.isEmpty()) {
+                fLogger.error("### Unable to find any records matching Credential ID: " + credId);
+                throw new RuntimeException("No records match Credential ID: " + credId);
+            } else if (records.size() > 1 && !recordsFilter.isEmpty()) {
+                fLogger.error("### Multiple records match Credential ID: " + credId);
+                throw new RuntimeException("Multiple records match Credential ID");
+            }
+
+            // find matching record
+            KeeperRecord record = null;
+            if (recordsFilter.isEmpty()) {
+                // no filter - search by type:title
+                List<KeeperRecord> found = new ArrayList<KeeperRecord>();
+                for (KeeperRecord rec : records) {
+                    boolean matchesType = isNullOrEmpty(recType) || recType.equals(rec.getType());
+                    boolean matchesTitle = recTitle.isEmpty() || recTitle.equals(rec.getTitle());
+                    if (matchesType && matchesTitle) {
+                        found.add(rec);
+                    }
+                }
+                if (found.size() == 1)
+                    record = found.get(0);
+                else if (found.isEmpty()) {
+                    fLogger.error("### Unable to find any records matching Credential ID: " + credId);
+                    throw new RuntimeException("No records match Credential ID: " + credId);
+                } else {
+                    List<String> ids = found.stream().map(KeeperRecord::getRecordUid).collect(Collectors.toList());
+                    fLogger.error("### Multiple records match Credential ID: " + credId + " - " + String.join(",", ids));
+                    throw new RuntimeException("Multiple records match Credential ID");
+                }
+            } else {
+                record = records.get(0);
+            }
+
+            // Grab the field values from the returned object
+            // NB! Always gets all fields with labels matching the prefix
+            // NB! Login records - always gets user/pass from corresponding fields ignoring any labels
+            if (record != null) {
+                // for Login records user/pass always come from corresponding record fields
+                if ("login".equalsIgnoreCase(record.getType())) {
+                    String password = record.getPassword();
+                    if (!isNullOrEmpty(password))
+                        result.put(VAL_PSWD, password);
+                    KeeperRecordField loginField = record.getData().getField(Login.class);
+                    if (loginField != null) {
+                        List<String> value = ((Login)loginField).getValue();
+                        if (!value.isEmpty())
+                            if (!isNullOrEmpty(value.get(0)))
+                                result.put(VAL_USER, value.get(0));
+                    }
+                }
+
+                // find fields by label prefix
+                List<KeeperRecordField> fields = record.getData().getCustom();
+                fields = (fields != null ? fields : new ArrayList<KeeperRecordField>());
+                for (KeeperRecordField field : fields) {
+                    String label = field.getLabel();
+                    if (!isNullOrEmpty(label) && label.startsWith(ksmLabelPrefix)){
+                        String key = label.substring(ksmLabelPrefix.length());
+                        if (isNullOrEmpty(key)) {
+                            fLogger.warn("### Skipped empty key for field: " + label);
+                        } else if (result.containsKey(key)) {
+                            fLogger.warn("### Skipped duplicate entry for field: " + label);
+                        } else {
+                            List<String> values = Collections.<String> emptyList();
+                            if (field instanceof Text){
+                                Text fld = (Text)field;
+                                values = fld.getValue();
+                            } else if (field instanceof HiddenField) {
+                                HiddenField fld = (HiddenField)field;
+                                values = fld.getValue();
+                            } else if (field instanceof Multiline) {
+                                Multiline fld = (Multiline)field;
+                                values = fld.getValue();
+                            } else {
+                                fLogger.error("### Skipped unexpected field type for field: " + label + " Only fields of type Text, Multiline or HiddenField are allowed.");
+                            }
+                            if (values.isEmpty())
+                                fLogger.warn("### Skipped empty field value for field: " + label);
+                            else {
+                                String val = values.get(0);
+                                if (isNullOrEmpty(val))
+                                    fLogger.warn("### Skipped empty field value for field: " + label);
+                                else
+                                    result.put(key, val);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            fLogger.error("### Unable to find credential from KSM App.", e);
+        }
+
+        if (!result.containsKey(VAL_USER))
+            fLogger.warn("### No value for username in credential: " + credId);
+        if (!result.containsKey(VAL_PSWD))
+            fLogger.warn("### No value for password in credential: " + credId);
+        fLogger.info("### Credential: " + credId + " Resolved keys: " + result.keySet());
+
+        return result;
+    }
+
+    private static boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+
+    //main method to test locally, provide KSM config and test it
+    // TODO: Remove this before moving to production
+    /*
+    public static void main(String[] args) {
+        CredentialResolver credResolver = new CredentialResolver();
+        credResolver.ksmConfig = "[Base64_KSM_Config]";
+        credResolver.ksmLabelPrefix = "mid_";
+
+        Map<String, String> map = new HashMap<>();
+        map.put(ARG_ID, "[RecordUid]");
+        map.put(ARG_TYPE, "ssh_password");
+
+        Map<String, String> result = credResolver.resolve(map);
+        fLogger.info(result.toString());
+    }
+    */
+}
