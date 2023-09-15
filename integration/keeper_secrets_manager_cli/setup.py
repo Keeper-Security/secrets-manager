bb6a1d67304d6f05aacfe63897ad6170133f9d83
Max Ustinov <mustinov@keepersecurity.com>
2023-09-15 16:54:48 -0700

    Update README, bumped to version 1.1.1, and bumped `keeper-secrets-manager-core` to 16.6.2 and `keeper-secrets-manager-storage` to 1.0.2

--- integration/keeper_secrets_manager_cli/setup.py
+++ integration/keeper_secrets_manager_cli/setup.py
@@ -27,7 +27,7 @@ install_requires = [
 # Version set in the keeper_secrets_manager_cli.version file.
 setup(
     name="keeper-secrets-manager-cli",
-    version="1.1.0",
+    version="1.1.1",
     description="Command line tool for Keeper Secrets Manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
