4a7fae7af49997c189d2f08f468448f7ac081a07
Max Ustinov <maksimu@users.noreply.github.com>
2024-09-18 15:22:04 -0700

    Release Azure Pipeline Extension v1.0.6 (#658)
    * update package dependencies and version numbers (#610)
    
    * KSM-544 - Added search by title (#636)

--- integration/keeper_secrets_manager_azure_pipeline_extension/ksm-azure-devops-secrets-task/tests/_suite.ts
+++ integration/keeper_secrets_manager_azure_pipeline_extension/ksm-azure-devops-secrets-task/tests/_suite.ts
@@ -1,11 +1,8 @@
 import * as path from 'path';
 import * as assert from 'assert';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
-import nock from 'nock';
-
 
 describe('Sample task tests', function () {
-
     before( function() {
 
     });
@@ -15,21 +12,22 @@ describe('Sample task tests', function () {
     });
 
     it('should succeed with simple inputs', function(done: Mocha.Done) {
-
         this.timeout(1000);
 
-        let tp = path.join(__dirname, 'success.js');
+        let tp: string = path.join(__dirname, 'success.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-
-        console.log(tr.succeeded);
-        assert.equal(tr.succeeded, true, 'should have succeeded');
-        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
-        assert.equal(tr.errorIssues.length, 0, "should have no errors");
-        console.log(tr.stdout);
-        // assert.equal(tr.stdout.indexOf('Hello human') >= 0, true, "should display Hello human");
-        done();
+        tr.runAsync().then(() => {
+            console.log(tr.succeeded);
+            assert.equal(tr.succeeded, true, 'should have succeeded');
+            assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+            assert.equal(tr.errorIssues.length, 0, "should have no errors");
+            console.log(tr.stdout);
+            // assert.equal(tr.stdout.indexOf('Hello human') >= 0, true, "should display Hello human");
+            done();
+        }).catch((error) => {
+            done(error); // Ensure the test case fails if there's an error
+        });
 
     });
 
