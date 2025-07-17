12b0a8acad717a96706c7b53230b96b9a3d63c6d
Ivan Dimov <78815270+idimov-keeper@users.noreply.github.com>
2025-07-17 15:15:58 -0500

    downgrade foojay-resolver to 0.9 for compatibility with current GHA java-version matrix

--- sdk/java/core/settings.gradle.kts
+++ sdk/java/core/settings.gradle.kts
@@ -1,7 +1,10 @@
 rootProject.name = "core"
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver") version "1.0.0"
+    // Note: Versions prior to 1.0.0 require Java 8 or later and Gradle 7.6 or later.
+    // Versions 1.0.0 and after require Java 17 or later and Gradle 7.6 or later.
+    // Upgrade to 1.0.0+ entails moving java-version matrix out of GHA and implementing in Gradle
+    id("org.gradle.toolchains.foojay-resolver") version "0.9.0"
 }
 
 @Suppress("UnstableApiUsage")
