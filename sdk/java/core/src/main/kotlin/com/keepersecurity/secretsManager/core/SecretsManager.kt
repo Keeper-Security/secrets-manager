b0102aa2e8c4fb9c7606f29896c7f583e3fd740a
idimov-keeper <78815270+idimov-keeper@users.noreply.github.com>
2025-09-05 15:20:45 -0500

    KSM-645 Added fat JAR Gradle task (#805)
    - bumped version to 17.1.2
    - added fat JAR build option
    - updated dependencies

--- sdk/java/core/src/main/kotlin/com/keepersecurity/secretsManager/core/SecretsManager.kt
+++ sdk/java/core/src/main/kotlin/com/keepersecurity/secretsManager/core/SecretsManager.kt
@@ -19,7 +19,7 @@ import java.util.*
 import java.util.concurrent.*
 import javax.net.ssl.*
 
-const val KEEPER_CLIENT_VERSION = "mj17.1.1"
+const val KEEPER_CLIENT_VERSION = "mj17.1.2"
 
 const val KEY_HOSTNAME = "hostname" // base url for the Secrets Manager service
 const val KEY_SERVER_PUBIC_KEY_ID = "serverPublicKeyId"
