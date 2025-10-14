c30fc43e605d00281dcc6dcc4baad37038eaeae0
Stas Schaller <sschaller@keepersecurity.com>
2025-10-14 16:33:00 -0400

    Updated workflow with inline sbom scanning and safer version checks

--- integration/keeper_secrets_manager_chef/cookbooks/keeper_secrets_manager/metadata.rb
+++ integration/keeper_secrets_manager_chef/cookbooks/keeper_secrets_manager/metadata.rb
@@ -10,10 +10,10 @@ chef_version '>= 16.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/keeper_secrets_manager/issues'
+issues_url 'https://github.com/Keeper-Security/secrets-manager/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/keeper_secrets_manager'
+source_url 'https://github.com/Keeper-Security/secrets-manager'
