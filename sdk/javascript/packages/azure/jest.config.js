4024ba01dab02faaa3f2a098de3a7f2ca589bfee
Stas Schaller <sschaller@keepersecurity.com>
2025-11-21 15:52:44 -0500

    Add test workflow and simplify Jest configuration

--- sdk/javascript/packages/azure/jest.config.js
+++ sdk/javascript/packages/azure/jest.config.js
@@ -5,14 +5,5 @@ module.exports = {
     "transform": {
         "^.+\\.(ts|tsx)$": "ts-jest"
     },
-    "testEnvironment": "node",
-    "collectCoverage": false,
-    "coverageThreshold": {
-        "global": {
-            "branches": 80,
-            "functions": 80,
-            "lines": 80,
-            "statements": 80
-        }
-    }
+    "testEnvironment": "node"
 }
