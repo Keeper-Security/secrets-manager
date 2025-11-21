69362c1858a2067351735c2d32f0be6380c932d6
satish-metron <138431431+satish-metron@users.noreply.github.com>
2025-11-21 22:53:31 +0530

    Added GCP KMS support to KSM JavaScript SDK (#719)
    * Bump version
    
    * fix: resolve DOM Clobbering CVE-2024-43788 (#689)
    
    * fix: resolve DOM Clobbering CVE-2024-43788
    
    * Upgrade JavaScript tests GHA to use Node 20
    
    * fixed moduleResolution in tsconfig
    updated all dependencies
    
    * Added GCP KMS support to KSM Javascript SDK
    
    * Added RAW_Encrypt functionality
    
    * Revert "Added RAW_Encrypt functionality"
    
    This reverts commit f6616bebef6b89484b4567b590b378193246ab08.
    
    * * Fixed issues related to writing to file even when encryption failed
    * Added support for GCP Raw Encrypt Decrypt type of key
    
    ---------
    
    Co-authored-by: Ivan Dimov <78815270+idimov-keeper@users.noreply.github.com>
    Co-authored-by: satish_chandra <metron_satish@safebreach.com>
    Co-authored-by: Ayush Asati <ayush.asati@metronlabs.com>

---
+++ sdk/javascript/packages/gcp/src/constants.ts
@@ -0,0 +1,26 @@
+import { KeyPurpose, LoggerLogLevelOptions } from "./enum";
+import { constants } from "crypto";
+
+export const supportedKeyPurpose: string[] = [
+  KeyPurpose.RAW_ENCRYPT_DECRYPT,
+  KeyPurpose.ENCRYPT_DECRYPT,
+  KeyPurpose.ASYMMETRIC_DECRYPT,
+];
+
+export const BLOB_HEADER = "\xff\xff"; // Encrypted BLOB Header: U+FFFF is a non character
+export const LATIN1_ENCODING = "latin1";
+export const UTF_8_ENCODING = "utf-8";
+export const AES_256_GCM = "aes-256-gcm";
+export const MD5_HASH = "md5";
+export const HEX_DIGEST = "hex";
+export const DEFAULT_JSON_INDENT = 4;
+export const OAEP_PADDINg = constants.RSA_PKCS1_OAEP_PADDING;
+export const SHA_256 = 'sha256';
+export const SHA_1 = 'sha1';
+export const SHA_512 = 'sha512';
+export const DEFAULT_LOG_LEVEL = LoggerLogLevelOptions.info;
+export const ADDITIONAL_AUTHENTICATION_DATA = "keeper_auth";
+export const TOKEN_SCOPE = "https://www.googleapis.com/auth/cloud-platform";
+export const RAW_ENCRYPT_GCP_API_URL = "https://cloudkms.googleapis.com/v1/{}:rawEncrypt";
+export const RAW_DECRYPT_GCP_API_URL = "https://cloudkms.googleapis.com/v1/{}:rawDecrypt";
+export const SCOPES = [TOKEN_SCOPE];
