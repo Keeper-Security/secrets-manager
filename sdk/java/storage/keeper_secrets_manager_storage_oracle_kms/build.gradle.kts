b786cb9cdb49d84a79555181449405ae3fb52465
Stas Schaller <sschaller@keepersecurity.com>
2025-11-25 11:38:41 -0500

    Add copyDependencies task for SBOM generation

--- sdk/java/storage/keeper_secrets_manager_storage_oracle_kms/build.gradle.kts
+++ sdk/java/storage/keeper_secrets_manager_storage_oracle_kms/build.gradle.kts
@@ -81,6 +81,11 @@ tasks.jar {
     }
 }
 
+tasks.register<Copy>("copyDependencies") {
+    from(configurations.runtimeClasspath)
+    into(layout.buildDirectory.dir("sbom-deps"))
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
