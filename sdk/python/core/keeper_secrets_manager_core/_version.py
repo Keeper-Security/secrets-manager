dd96c408c2eb05c048c198f297a625f52e09345f
Stas Schaller <sschaller@keepersecurity.com>
2026-01-16 22:14:51 -0500

    KSM-749: fix client version detection from stale .dist-info metadata

---
+++ sdk/python/core/keeper_secrets_manager_core/_version.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#  _  __
+# | |/ /___ ___ _ __  ___ _ _ (R)
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|
+#
+# Keeper Secrets Manager
+# Copyright 2023 Keeper Security Inc.
+# Contact: sm@keepersecurity.com
+
+# Single source of truth for package version
+# This file is imported by both setup.py and the package itself
+__version__ = "17.1.0"
