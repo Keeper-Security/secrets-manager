b39d0b8aababdab16e6b254ff8d4fd839e8cb24c
Stas Schaller <sschaller@keepersecurity.com>
2026-01-21 12:29:31 -0500

    KSM-758: replace rollup-plugin-sourcemaps with rollup-plugin-sourcemaps2

--- sdk/javascript/packages/core/rollup.config.js
+++ sdk/javascript/packages/core/rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from "rollup-plugin-typescript2"
 import pkg from './package.json'
-import sourcemaps from "rollup-plugin-sourcemaps";
+import sourcemaps from "rollup-plugin-sourcemaps2";
 import versionInjector from "rollup-plugin-version-injector";
 
 export default [
